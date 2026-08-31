63a36cd6fb9527d2def04011f9d36200d559c631
samuderapang <samuderapanggabean@gmail.com>
2026-08-31 10:14:35 +0700

    W03: Jawaban soal 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
-import Profile from "../components/profile";
+import { Gallery } from "../components/gallery"; 
+// Catatan: Jika di modul menggunakan "@/components/gallery" dan terjadi error merah, gunakan "../components/gallery" seperti baris di atas karena sebelumnya Anda tidak menggunakan folder 'src'.
 
 export default function Home() {
   return (
     <main style={{ padding: "20px" }}>
       <h2>Ilmuwan yang luar biasa</h2>
-      <div style={{ display: "flex" }}>
-        <Profile />
-        <Profile />
-        <Profile />
-      </div>
+      <Gallery />
     </main>
   );
 }
